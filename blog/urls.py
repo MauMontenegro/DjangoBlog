a5f7c71c5a56b4cfd681427fd2f3155d56e326f4
mauro <m.montenegro.meza@gmail.com>
2024-04-26 14:12:25 -0600

    Added a Post_detail view and template extensions

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # Add first URL pattern
 urlpatterns=[
-    path('',views.post_list, name='post_list')
+    path('',views.post_list, name='post_list'),
+    path('post/<int:pk>',views.post_detail,name='post_detail')
 ]
